6bd1ea25eaaf90a3fa5e1e0bd56b9de905e1d499
“HectorDevelops” <HectorDevelops@gmail.com>
2023-05-01 16:57:16 -0700

    updated comments

--- Program.cs
+++ Program.cs
@@ -1,42 +1,42 @@
 ﻿
-// // FUNDAMENTALS PRACTICE ASSIGNMENT 
+// FUNDAMENTALS PRACTICE ASSIGNMENT 
 
-// // 1. Create a loop that prints all values from 1-255.
-// for (int i = 0; i <= 255; i++)
-// {
-//     Console.WriteLine(i);
-// }
+// 1. Create a loop that prints all values from 1-255.
+for (int i = 0; i <= 255; i++)
+{
+    Console.WriteLine(i);
+}
 
-// // 2. Create a new loop that generates 5 random numbers between 10 and 20.
-// Random arbitraree = new Random();
-// for (int i = 0; i < 5; i++)
-// {
-//     Console.WriteLine(arbitraree.Next(2, 8));
-// }
+// 2. Create a new loop that generates 5 random numbers between 10 and 20.
+Random arbitraree = new Random();
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(arbitraree.Next(2, 8));
+}
 
-// // Modify the previous loop to add the random values together and print the sum after the loop finishes.
-// Random arbitrary = new Random();
-// int totalSum = 0;
-// for (int i = 0; i < 5; i++)
-// {
-//     totalSum += arbitrary.Next(5, 10);
-// }
-// Console.WriteLine($"The total sum of the loop is {totalSum}");
+// 3. Modify the previous loop to add the random values together and print the sum after the loop finishes.
+Random arbitrary = new Random();
+int totalSum = 0;
+for (int i = 0; i < 5; i++)
+{
+    totalSum += arbitrary.Next(5, 10);
+}
+Console.WriteLine($"The total sum of the loop is {totalSum}");
 
 
-// // Create a new loop that prints all values from 1 to 100 that are divisible by 3 OR 5, but NOT both.
+// 4. Create a new loop that prints all values from 1 to 100 that are divisible by 3 OR 5, but NOT both.
 
-// for (int i = 0; i <= 100; i++)
-// {
-//     bool isDivisibleBy3 = i % 3 == 0;
-//     bool isDivisibleBy5 = i % 5 == 0;
-//     if( isDivisibleBy3 || isDivisibleBy5)
-//     {
-//         Console.WriteLine($"{i} is divisible by 3 or 5.");
-//     }
-// }
+for (int i = 0; i <= 100; i++)
+{
+    bool isDivisibleBy3 = i % 3 == 0;
+    bool isDivisibleBy5 = i % 5 == 0;
+    if( isDivisibleBy3 || isDivisibleBy5)
+    {
+        Console.WriteLine($"{i} is divisible by 3 or 5.");
+    }
+}
 
-// Modify the previous loop to print "Fizz" for multiples of 3 and "Buzz" for multiples of 5.
+// 5. Modify the previous loop to print "Fizz" for multiples of 3 and "Buzz" for multiples of 5.
 for (int i = 0; i <= 100; i++)
 {
     bool isDivisibleBy3 = i % 3 == 0;
@@ -54,7 +54,7 @@ for (int i = 0; i <= 100; i++)
         Console.WriteLine(i);
     }
 }
-// Modify the previous loop once more to now also print "FizzBuzz" for numbers that are multiples of both 3 and 5.
+// 6. Modify the previous loop once more to now also print "FizzBuzz" for numbers that are multiples of both 3 and 5.
 for (int i = 0; i <= 100; i++)
 {
     bool isDivisibleBy3 = i % 3 == 0;
@@ -76,10 +76,3 @@ for (int i = 0; i <= 100; i++)
         Console.WriteLine(i);
     }
 }
-
-
-
-// (Optional) If you used for loops for your solutions, try doing the same with while loops. Vice versa if you used while loops!
-
-
-
